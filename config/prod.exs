ae0bdca47bd61955554c0c32662efab5689d39d5
seanreed1111 <seanreed1111@gmail.com>
2016-09-27 12:33:57 -0400

    corrects pool_size expression again

--- config/prod.exs
+++ config/prod.exs
@@ -27,4 +27,5 @@ config :logger, level: :info
 config :hello_phoenix, HelloPhoenix.Repo,
 adapter: Ecto.Adapters.Postgres,
 url: System.get_env("DATABASE_URL"),
-pool_size: String.to_integer(System.get_env("POOL_SIZE" || "10"), ssl: true
+pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"), 
+ssl: true
