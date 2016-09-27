80c9075f528506ae01d649c355f23d57137a28ab
seanreed1111 <seanreed1111@gmail.com>
2016-09-27 12:27:58 -0400

    extra comma removed

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :hello_phoenix, HelloPhoenix.Endpoint,
   url: [scheme: "https", host: "sheltered-atoll-94626.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
 # Do not print debug messages in production
@@ -29,5 +29,3 @@ adapter: Ecto.Adapters.Postgres,
 url: System.get_env("DATABASE_URL"),
 pool_size: String.to_integer(System.get_env("POOL_SIZE" || 10),
 ssl: true
-
-
